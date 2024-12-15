bb80f08c3a1bd92e4bd90c1cafba455b4bcfe79c
Stralax <strahinjadjordjevic2003@gmail.com>
2024-12-15 19:36:20 +0100

    new start

--- assets/js/auctionAlgorithm.js
+++
@@ -1,5 +0,0 @@
-export function nextPrice(currentPrice, maxBid) {
-    console.log(maxBid);
-    return Math.min(currentPrice + 0.1 * maxBid, maxBid);
-}
-
--- assets/js/profileDynamic.js
+++
@@ -1,5 +0,0 @@
-export let fullName = "Andrej Bojchevski"
-export let lastOnline = "13.05.2003"
-export let joined = "01.05.2022"
-export let numFriends = 3
-export let numViews = 9
