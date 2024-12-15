48d43d42c0c217d92d822979c2f53aa1d8076378
Stralax <strahinjadjordjevic2003@gmail.com>
2024-12-15 19:46:35 +0100

    version 2.0

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Streži statične datoteke iz Angular aplikacije
+app.use(express.static('./'));
+
+// Preusmeri vse poti na `index.html` (za Angular router)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join('./'));
+});
+
+// Zaženi strežnik
+app.listen(port, () => {
+  console.log(`Angular aplikacija je dostopna na http://localhost:${port}`);
+});
